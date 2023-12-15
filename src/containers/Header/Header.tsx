920ddb79bc2645cbfc09f17f066472fcee19f62a
luckmer <woszczo04@gmail.com>
2023-12-15 17:59:19 +0100

    new header ui

---
+++ src/containers/Header/Header.tsx
@@ -0,0 +1,18 @@
+import { useLocation, useNavigate } from '@solidjs/router'
+import MainHeader from '@components/Header'
+import { stepStatus } from '@src/static'
+import { DIRECTION } from '@src/static/types/enums'
+
+export const Header = () => {
+    const location = useLocation()
+    const navigate = useNavigate()
+    return (
+        <MainHeader
+            name="Welcome!"
+            step={stepStatus[DIRECTION[location.pathname]]}
+            onClick={() => {
+                navigate('/')
+            }}
+        />
+    )
+}
