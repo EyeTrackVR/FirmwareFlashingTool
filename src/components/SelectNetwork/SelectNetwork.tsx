69f7a7eaad2da7d1fe4b93ccd039024d782064a9
luckmer <woszczo04@gmail.com>
2023-12-21 19:30:45 +0100

    added auto complete

--- src/components/SelectNetwork/SelectNetwork.tsx
+++ src/components/SelectNetwork/SelectNetwork.tsx
@@ -10,13 +10,17 @@ export interface IProps {
 
 export const SelectNetwork: Component<IProps> = (props) => {
     return (
-        <div class="flex flex-col gap-[10px] w-[285px] p-[24px] rounded-[12px] border border-solid border-[#192736] bg-[#0D1B26]">
+        <form
+            action="#"
+            class="flex flex-col gap-[10px] w-[285px] p-[24px] rounded-[12px] border border-solid border-[#192736] bg-[#0D1B26]">
             <div class="flex flex-col gap-[10px]">
                 <div class="text-left text-[14px] text-white font-[500] leading-[14px] not-italic">
                     SSID
                 </div>
                 <div>
                     <Input
+                        id="firstName"
+                        autoComplete="given-name"
                         required={true}
                         autoFocus={true}
                         type="text"
@@ -32,6 +36,8 @@ export const SelectNetwork: Component<IProps> = (props) => {
                 </div>
                 <div>
                     <Input
+                        id="password"
+                        autoComplete="current-password"
                         required={true}
                         type="password"
                         onChange={props.onChangePassword}
@@ -40,6 +46,6 @@ export const SelectNetwork: Component<IProps> = (props) => {
                     />
                 </div>
             </div>
-        </div>
+        </form>
     )
 }
