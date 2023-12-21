87f7e17fff5b52ae0baa1ae907497e7a6468727f
luckmer <woszczo04@gmail.com>
2023-12-21 19:31:09 +0100

    added few optional values

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,13 @@
-import { Component } from 'solid-js'
+import { Component, Show } from 'solid-js'
 import { Button } from '@components/Buttons/DefaultButton'
 
 export interface IProps {
-    onClickSecond: () => void
-    onClickPrimary: () => void
-    isPrimaryActive: boolean
-    isSecondActive: boolean
-    primaryLabel: string
-    secondLabel: string
+    onClickSecond?: () => void
+    onClickPrimary?: () => void
+    isPrimaryActive?: boolean
+    isSecondActive?: boolean
+    primaryLabel?: string
+    secondLabel?: string
     primaryType?: 'submit' | 'reset' | 'button' | undefined
     secondType?: 'submit' | 'reset' | 'button' | undefined
 }
@@ -15,18 +15,22 @@ export interface IProps {
 export const Footer: Component<IProps> = (props) => {
     return (
         <div class="flex w-full pb-[24px] pr-[40px] pl-[40px] justify-end gap-[10px]">
-            <Button
-                isActive={false}
-                type={props.secondType}
-                label={props.secondLabel}
-                onClick={props.onClickSecond}
-            />
-            <Button
-                isActive={!props.isPrimaryActive}
-                type={props.primaryType}
-                label={props.primaryLabel}
-                onClick={props.onClickPrimary}
-            />
+            <Show when={props.onClickSecond}>
+                <Button
+                    isActive={false}
+                    type={props.secondType}
+                    label={props.secondLabel ?? ''}
+                    onClick={props.onClickSecond}
+                />
+            </Show>
+            <Show when={props.onClickPrimary}>
+                <Button
+                    isActive={!props.isPrimaryActive}
+                    type={props.primaryType}
+                    label={props.primaryLabel ?? ''}
+                    onClick={props.onClickPrimary}
+                />
+            </Show>
         </div>
     )
 }
