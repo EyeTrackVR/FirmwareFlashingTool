52487b98cfbc8aea8cfc9ec6eabbd7b7c02c5c6f
luckmer <woszczo04@gmail.com>
2024-03-10 13:53:53 +0100

    connected clsx

--- src/components/Buttons/DefaultButton/index.tsx
+++ src/components/Buttons/DefaultButton/index.tsx
@@ -1,3 +1,4 @@
+import { clsx } from 'clsx'
 import { Component, createMemo } from 'solid-js'
 
 export interface IProps {
@@ -11,25 +12,28 @@ export interface IProps {
 export const Button: Component<IProps> = (props) => {
     const styles = createMemo(() => {
         if (props.isLoadingPrimaryButton) {
-            return 'bg-[#192736] pr-[32px] pl-[32px] pt-[8px] pb-[8px] rounded-[6px] border-solid border-1 border-[#192736] focus-visible:border-[#817DF7] cursor-wait'
+            return 'bg-[#192736] border-[#192736] focus-visible:border-[#817DF7] cursor-wait'
         }
         if (typeof props.isActive !== 'undefined') {
             return props.isActive
-                ? 'bg-[#817DF7] pr-[32px] pl-[32px] pt-[8px] pb-[8px] rounded-[6px] hover:bg-[#9793FD] border-[#192736] border-solid border-1 focus-visible:border-[#fff]'
-                : 'bg-[#192736] pr-[32px] pl-[32px] pt-[8px] pb-[8px] rounded-[6px] hover:bg-[#30475e] border-solid border-1 border-[#192736] focus-visible:border-[#817DF7]'
+                ? 'bg-[#817DF7] hover:bg-[#9793FD] border-[#192736] focus-visible:border-[#fff]'
+                : 'bg-[#192736] hover:bg-[#30475e] border-[#192736] focus-visible:border-[#817DF7]'
         }
-        return 'bg-[#192736] pr-[32px] pl-[32px] pt-[8px] pb-[8px] rounded-[6px] hover:bg-[#30475e] border-solid border-1 border-[#192736] focus-visible:border-[#817DF7]'
+        return 'bg-[#192736] hover:bg-[#30475e] border-[#192736] focus-visible:border-[#817DF7]'
     })
 
     return (
         <button
             type={props.type}
-            class={styles()}
+            class={clsx(
+                'pr-[32px] pl-[32px] pt-[8px] pb-[8px] rounded-[6px] border-solid border-1',
+                styles(),
+            )}
             onClick={(e) => {
                 e.preventDefault()
                 props.onClick?.()
             }}>
-            <p class="text-left text-[14px] text-white font-[700] leading-[20px] not-italic">
+            <p class="text-left text-[14px] text-white font-normal leading-[20px] not-italic whitespace-nowrap">
                 {props.label}
             </p>
         </button>
