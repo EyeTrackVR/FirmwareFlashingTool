41790cad8b4aca9ac1367891f314284b30a75f8a
luckmer <woszczo04@gmail.com>
2024-03-13 19:13:26 +0100

    added correct icons for titlebar

--- src/components/Titlebar/Titlebar.tsx
+++ src/components/Titlebar/Titlebar.tsx
@@ -7,33 +7,35 @@ export interface IProps {
 
 export const Titlebar: Component<IProps> = (props) => {
     return (
-        <div data-tauri-drag-region class="titlebar">
+        <div data-tauri-drag-region class="customTitlebar">
             <div
                 class="titlebar-button"
                 onClick={() => {
                     props.onClickHeader(TITLEBAR_ACTION.MINIMIZE)
                 }}>
-                <img
-                    src="https://api.iconify.design/mdi:window-minimize.svg"
-                    alt={TITLEBAR_ACTION.MINIMIZE}
-                />
+                <svg width="1em" height="1em" viewBox="0 0 24 24">
+                    <path fill="#ffffff" d="M20 14H4v-4h16" />
+                </svg>
             </div>
             <div
                 class="titlebar-button"
                 onClick={() => {
                     props.onClickHeader(TITLEBAR_ACTION.MAXIMIZE)
                 }}>
-                <img
-                    src="https://api.iconify.design/mdi:window-maximize.svg"
-                    alt={TITLEBAR_ACTION.MAXIMIZE}
-                />
+                <svg width="1em" height="1em" viewBox="0 0 24 24">
+                    <path fill="#ffffff" d="M4 4h16v16H4zm2 4v10h12V8z" />
+                </svg>
             </div>
             <div
                 class="titlebar-button"
                 onClick={() => {
                     props.onClickHeader(TITLEBAR_ACTION.CLOSE)
                 }}>
-                <img src="https://api.iconify.design/mdi:close.svg" alt={TITLEBAR_ACTION.CLOSE} />
+                <svg width="1em" height="1em" viewBox="0 0 24 24">
+                    <path
+                        fill="white"
+                        d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12z" />
+                </svg>
             </div>
         </div>
     )
