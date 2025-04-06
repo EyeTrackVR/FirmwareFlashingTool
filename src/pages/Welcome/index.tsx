3b5919ea1a0e798a1c8fc69498c2039bdffb4dbc
luckmer <woszczo04@gmail.com>
2025-04-06 17:58:27 +0200

    center welcome page

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -10,35 +10,39 @@ export interface IProps {
 
 const Welcome: Component<IProps> = (props) => {
     return (
-        <div class="w-full h-full flex flex-col items-center justify-center gap-32">
-            <Image.Root class="select-none">
-                <Image.Img
-                    src="images/logo.png"
-                    alt="logo"
-                    width="128px"
-                    class="min-w-[128px] min-h-[128px]"
-                />
-            </Image.Root>
-            <Typography text="h1" color="white">
-                Welcome Traveler!
-            </Typography>
-            <div class="flex flex-col gap-6">
-                <Button
-                    disabled={false}
-                    isLoadingPrimaryButton={false}
-                    isActive={true}
-                    type="button"
-                    label="Full ETVR Setup"
-                    onClick={props.onClickSetup}
-                />
-                <Button
-                    disabled={false}
-                    isLoadingPrimaryButton={false}
-                    isActive={false}
-                    type="button"
-                    label="Add Existing boards"
-                    onClick={props.onClickAddExisting}
-                />
+        <div class="w-full h-full  flex flex-col items-center justify-center gap-32">
+            <div class="flex scale-[2.8]">
+                <div class="flex flex-col items-center justify-center scale-[0.42] gap-32 mb-[40px]">
+                    <Image.Root class="select-none">
+                        <Image.Img
+                            src="images/logo.png"
+                            alt="logo"
+                            width="128px"
+                            class="min-w-[64px] min-h-[64px]"
+                        />
+                    </Image.Root>
+                    <Typography text="h1" color="white">
+                        Welcome To EyeTrackVR
+                    </Typography>
+                    <div class="flex flex-col gap-6">
+                        <Button
+                            disabled={false}
+                            isLoadingPrimaryButton={false}
+                            isActive={true}
+                            type="button"
+                            label="Full ETVR Setup"
+                            onClick={props.onClickSetup}
+                        />
+                        <Button
+                            disabled={false}
+                            isLoadingPrimaryButton={false}
+                            isActive={false}
+                            type="button"
+                            label="Add Existing boards"
+                            onClick={props.onClickAddExisting}
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     )
