54c28a45e6a5e25f10ff0b231d15a6473244de23
luckmer <woszczo04@gmail.com>
2025-06-08 13:56:19 +0200

    general settings

--- src/pages/Settings/GeneralSettings/index.tsx
+++ src/pages/Settings/GeneralSettings/index.tsx
@@ -1,8 +1,4 @@
 import Button from '@components/Buttons/Button'
-import { ToggleButton } from '@components/Buttons/ToggleButton'
-import Typography from '@components/Typography'
-import ContextWrapper from '@components/Wrapper/ContextWrapper'
-import { FaSolidArrowsToEye } from 'solid-icons/fa'
 
 const GeneralSettings = () => {
     return (
@@ -10,20 +6,6 @@ const GeneralSettings = () => {
             <div class="w-full flex flex-row justify-end ">
                 <Button label="Reset settings to default" isDangerous />
             </div>
-            <ContextWrapper
-                icon={FaSolidArrowsToEye}
-                iconColor="white"
-                label="Eye falloff settings"
-                description="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.">
-                <div class="flex flex-col">
-                    <div class="flex flex-row items-center gap-6">
-                        <ToggleButton onToggle={() => {}} isToggled={false} />
-                        <Typography color="white" text="caption" nowrap>
-                            Outer Eye Falloff
-                        </Typography>
-                    </div>
-                </div>
-            </ContextWrapper>
         </div>
     )
 }
